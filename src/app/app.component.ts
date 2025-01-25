d4998fa32e59a2443d18019987a45913cef13454
ahmedessawy10 <elessawy238@gmail.com>
2025-01-26 00:47:20 +0200

    design cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HOMEComponent } from './components/home/home.component';
 import { HEADERComponent } from './components/header/header.component';
 import { FOOTERComponent } from './components/footer/footer.component';
 import { ParentComponent } from './components/parent/parent.component';
+import { ORDERComponent } from './components/order/order.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HEADERComponent, HOMEComponent, FOOTERComponent, ParentComponent],
+  imports: [RouterOutlet, HEADERComponent, HOMEComponent, FOOTERComponent, ParentComponent, ORDERComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
